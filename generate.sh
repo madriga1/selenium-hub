9ea1644c3ec9e920160af53f2d8682f406836fb8
madriga1 <madriga1@msn.com>
2017-09-24 07:57:39 -0500

    adds the from information to dockerfile

--- generate.sh
+++ generate.sh
@@ -6,7 +6,7 @@ echo "# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" > ./Dockerfile
 echo "# NOTE: DO *NOT* EDIT THIS FILE.  IT IS GENERATED." >> ./Dockerfile
 echo "# PLEASE UPDATE Dockerfile.txt INSTEAD OF THIS FILE" >> ./Dockerfile
 echo "# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" >> ./Dockerfile
-echo FROM $NAMESPACE/base >> ./Dockerfile
+echo FROM $NAMESPACE >> ./Dockerfile
 echo LABEL authors="$AUTHORS" >> ./Dockerfile
 echo "" >> ./Dockerfile
 cat ./Dockerfile.txt >> ./Dockerfile
